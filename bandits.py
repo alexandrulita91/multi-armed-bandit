340d7221d4dada2fe3ce945437d0d09691cbe8bb
Trusty Coder <trustycoder83@gmail.com>
2020-06-10 22:23:28 +0300

    Update bandits.py
    Improve solution

--- bandits.py
+++ bandits.py
@@ -45,66 +45,60 @@ if __name__ == '__main__':
     neg_rewards = np.zeros(d)
 
     # Taking our best slot machine through beta distribution
-    for i in range(N):
-        selected = 0
-        max_random = 0
-        for j in range(d):
-            random_beta = np.random.beta(pos_rewards[j] + 1, neg_rewards[j] + 1)
-            if random_beta > max_random:
-                max_random = random_beta
-                selected = j
-
-        # Update the losses and wins
-        if X[i][selected] == 1:
-            pos_rewards[selected] += 1
-        else:
-            neg_rewards[selected] += 1
-
-        # Clear the screen
-        screen.fill(0)
-
-        # Draw global stats
-        pygame.draw.line(screen, WHITE, (200, 0), (200, height))
-        screen.blit(game_font.render("Total rounds: {}".format(N), False, WHITE), (20, 15))
-        screen.blit(game_font.render("Round: {}".format(i + 1), False, WHITE), (20, 35))
-
-        # Draw slot machines stats
-        for j in range(d):
-            screen.blit(game_font.render("Slot machine nr. {}".format(j + 1),
-                                         False, WHITE), (20, 75 + 60 * j))
-            screen.blit(game_font.render("Wins / Losses: {} / {}".format(int(pos_rewards[j]), int(neg_rewards[j])),
-                                         False, WHITE), (20, 95 + 60 * j))
-
-        # If all the rounds are completed, we will mark the best slot machine
-        if i == N - 1:
-            selected = np.argmax(pos_rewards + neg_rewards)
-
-        # Draw the slot machines
-        for j in range(d):
-            if j != selected:
-                screen.blit(slot_machine_idle, (240 + 200 * j, 10))
-            else:
-                if i == N - 1:
-                    screen.blit(slot_machine_best, (240 + 200 * j, 10))
-                elif X[i][j] == 1:
-                    screen.blit(slot_machine_win, (240 + 200 * j, 10))
-                else:
-                    screen.blit(slot_machine_lose, (240 + 200 * j, 10))
-
-        # Update the screen
-        pygame.display.flip()
-
-        # Handle any incoming event
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                exit(0)
-
-        # Sleep for a given time to display a nice animation
-        pygame.time.wait(250)
-
-    # Wait until we close the game
+    i = 0
     while True:
+        if i < N:
+            selected = 0
+            max_random = 0
+            for j in range(d):
+                random_beta = np.random.beta(pos_rewards[j] + 1, neg_rewards[j] + 1)
+                if random_beta > max_random:
+                    max_random = random_beta
+                    selected = j
+
+            # Update the losses and wins
+            if X[i][selected] == 1:
+                pos_rewards[selected] += 1
+            else:
+                neg_rewards[selected] += 1
+
+            # Clear the screen
+            screen.fill(0)
+
+            # Draw global stats
+            pygame.draw.line(screen, WHITE, (200, 0), (200, height))
+            screen.blit(game_font.render("Total rounds: {}".format(N), False, WHITE), (20, 15))
+            screen.blit(game_font.render("Round: {}".format(i + 1), False, WHITE), (20, 35))
+
+            # Draw slot machines stats
+            for j in range(d):
+                screen.blit(game_font.render("Slot machine nr. {}".format(j + 1),
+                                             False, WHITE), (20, 75 + 60 * j))
+                screen.blit(game_font.render("Wins / Losses: {} / {}".format(int(pos_rewards[j]), int(neg_rewards[j])),
+                                             False, WHITE), (20, 95 + 60 * j))
+
+            # If all the rounds are completed, we will mark the best slot machine
+            if i == N - 1:
+                selected = np.argmax(pos_rewards + neg_rewards)
+
+            # Draw the slot machines
+            for j in range(d):
+                if j != selected:
+                    screen.blit(slot_machine_idle, (240 + 200 * j, 10))
+                else:
+                    if i == N - 1:
+                        screen.blit(slot_machine_best, (240 + 200 * j, 10))
+                    elif X[i][j] == 1:
+                        screen.blit(slot_machine_win, (240 + 200 * j, 10))
+                    else:
+                        screen.blit(slot_machine_lose, (240 + 200 * j, 10))
+
+            # Update the screen
+            pygame.display.flip()
+
+            # Next round
+            i = i + 1
+
         # Handle any incoming event
         for event in pygame.event.get():
             if event.type == QUIT:
